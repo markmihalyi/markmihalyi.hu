f76a176f8987b2d016bb075c6bb698553b7cdba5
Márk Mihályi <mail@markmihalyi.hu>
2022-05-03 10:19:11 +0200

    Create ismeretlen_fajl.sh

---
+++ ismeretlen_fajl.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+printf "\n*** Az ismeretlen eredetű fájl a merevlemezre került ***\n\n"
+
+printf "*** Ezeket az üzeneteket azért látod, mert egyből lefuttatta a BASH fájlt a rendszer a wget parancs után kiadott paraméterek végett. ***\n"
+
+printf "*** Ha itt most nem ezek a barátságos 'echo' sorok lennének, hanem mondjuk 'rm -rf /', akkor a rendszer pillanatok alatt kritikus\n"
+
+printf "állapotba kerülne, mivel ez a parancs az összes fájlt törölné a gépen, ráadásul bármiféle értesítés nélkül. ***\n"
+
+printf "*** Igen, még a rendszerfájlokat is. ***\n"
+
+printf "*** Ez utóbbihoz habár szükséges lehet a '--no-preserve-root' megadása a parancs végén. ***\n\n"
